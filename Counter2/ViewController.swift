01f8a72ef3f44c93961310ea83465d51eeaaf4fe
D7703_28 <d7703_28@D7703-28ui-iMac.local>
2018-03-20 15:56:42 +0900

    color change

--- Counter2/ViewController.swift
+++ Counter2/ViewController.swift
@@ -9,17 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var count = 0
+    
+    @IBOutlet weak var lbl01: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    @IBAction func btn01(_ sender: UIButton) {
+        count = count + 1
+        lbl01.text = "\(count)"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+      
     }
+    
+    @IBAction func btn02(_ sender: Any) {
+        count = 0
+        lbl01.text = "\(count)"
+    }
+    @IBAction func btn03(_ sender: Any) {
+        count = count + 1
+        if count % 2 == 0 {
+            view.backgroundColor = UIColor.green
+            
 
+        }
+        else {
+            view.backgroundColor = UIColor.blue
+        }
+    }
+    
 
 }
 
